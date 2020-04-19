e1be2a9e7fc7cc3b093b8f4821041c061b6fb501
Nicholas Whittaker <nchlswhttkr@gmail.com>
2020-04-19 14:39:05 +1000

    Update

--- lint-if-instructed.sh
+++ lint-if-instructed.sh
@@ -15,9 +15,9 @@ set -e
 
 if [ -n "$BARHACK_LINT_ID" ]; then
     # Attempt to upload a pipeline file, invalid files will fail
-    buildkite-agent pipeline upload /home/barhack/files/$BARHACK_LINT_ID/pipeline.yml
-    printf PASSED > /home/barhack/files/$BARHACK_LINT_ID/status.txt
-    curl \
-        -H "Authorization: Bearer $BARHACK_BUILDKITE_TOKEN" \
-        -X PUT "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_NUMBER/cancel"
+    buildkite-agent pipeline upload $BARHACK_FILE_ROOT/$BARHACK_LINT_ID/pipeline.yml
+    printf PASSED > $BARHACK_FILE_ROOT/$BARHACK_LINT_ID/status.txt
+
+    curl --fail -X PUT "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_NUMBER/cancel" \
+        -H "Authorization: Bearer $BARHACK_BUILDKITE_TOKEN"
 fi
